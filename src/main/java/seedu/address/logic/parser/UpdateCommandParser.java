09b9838e3cf0143b99f01574db8322f3db8c46ad
zhenghuil <e0959119@u.nus.edu>
2024-04-03 05:07:57 +0800

    checkstyle

--- src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
@@ -7,16 +7,10 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTES;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.UpdateCommand;
 import seedu.address.logic.commands.UpdateCommand.UpdateBookingDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new EditCommand object
