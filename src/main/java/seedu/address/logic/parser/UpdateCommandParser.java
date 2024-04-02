060ccd624724ab56279652a21ab03954c253d640
zhenghuil <e0959119@u.nus.edu>
2024-04-03 03:08:00 +0800

    checkstyle

--- src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
@@ -4,8 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTES;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
 
 import java.util.Collection;
 import java.util.Collections;
